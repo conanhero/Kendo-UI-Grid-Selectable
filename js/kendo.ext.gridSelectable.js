c26acb356355a838384d567d63d3d64491f198f4
triet.nm <petertriet@gmail.com>
2014-03-14 16:23:48 +0700

    Update ext

--- js/kendo.ext.gridSelectable.js
+++ js/kendo.ext.gridSelectable.js
@@ -1,78 +1,77 @@
-/**  
-  * @desc Kendo Custom Plugin Selectable Grid
-  * @author Triet Nguyen petertriet@gmail.com 
-  * @required jquery.min.js, kendo.web.min.js, kendo.custom.selectable.js
-*/  
-(function (kendo, $) {
+/**
+ * @desc Kendo Custom Plugin Selectable Grid
+ * @author Triet Nguyen petertriet@gmail.com
+ * @required jquery.min.js, kendo.web.min.js, kendo.custom.selectable.js
+ */
+(function(kendo, $) {
     var ExtGridSelectable = kendo.ui.Widget.extend({
-        _grid: null,
-        _gridID: null,
-        _chkClass: null,
-        _chkAllID: null,
-        _selectedRow: null,
-		
-        init: function (element, options) {
+        _grid : null,
+        _gridID : null,
+        _chkClass : null,
+        _chkAllID : null,
+        _selectedRow : null,
+
+        init : function(element, options) {
             var that = this;
 
             kendo.ui.Widget.fn.init.call(that, element, options);
-            that._gridID = "#" + $(element).attr('id');
+            that._gridID   = "#" + $(element).attr('id');
             that._chkClass = "." + $(options.chkTemplate).attr('class');
             that._chkAllID = "#" + (options.chkAllID !== "" && options.chkAllID !== undefined ? options.chkAllID : $(options.chkAllTemplate).attr('id'));
-            
+
             options.grid.columns.unshift({
-                title: "",
-                template: options.chkTemplate,
-                headerTemplate: options.chkAllTemplate,
-                attributes: {"class": "align-center"},
-                width: "30px",
-                sortable: false
+                title : "",
+                template : options.chkTemplate,
+                headerTemplate : options.chkAllTemplate,
+                attributes : {
+                    "class" : "align-center"
+                },
+                width : "30px",
+                sortable : false
             });
-            
+
             options.grid.dataBound = that.databound;
-            
+
             that._grid = $(element).kendoGrid(options.grid).data("kendoGrid");
             that._grid.thead.append(options.filterHeader);
-            that._grid.bind("change", function (e) {
+            that._grid.bind("change", function(e) {
                 $(this._gridID + " table " + this._chkClass + ":checked").prop("checked", true);
             });
-			
-            $(that._gridID + " table tbody tr").on({click: function(e) {
+
+            $(that._gridID + " table tbody tr").on({click : function(e) {
                 that._handleSingleClick(e);
             }});
-            
-            $(that._chkAllID).on({change:function(e){
-                that._handleSelectAll();
+
+            $(that._chkAllID).on({change : function(e) {
+                    that._handleSelectAll();
             }});
-			
-			
-			
-			var pager = that._grid.pager.bind("change", function(){
-				that._pageChange();
-			});
+
+            that._grid.pager.bind("change", function() {
+                that._pageChange();
+            });
         },
-        _handleSingleClick: function (e) {
+        _handleSingleClick : function(e) {
             var checkBox = e.target;
             var row = $(checkBox).parent().parent();
             var checkedAll = $(this._chkAllID).prop("checked");
-			
-            if($(checkBox).is("input[type=checkbox]")) { 
-                if($(checkBox).prop("checked")) {
-                    $(this._gridID + " table " + this._chkClass + ":checked").prop("checked", false);
+
+            if ($(checkBox).is("input[type=checkbox]")) {
+                if ($(checkBox).prop("checked")) {
+                    $( this._gridID + " table " + this._chkClass + ":checked").prop("checked", false);
                     var selected = this._grid.select();
-					console.log(selected);
-					$(selected).find(this._chkClass).prop("checked", true);
-					this._grid.select(selected);
-                } else {					
-					var count = $(this._selectedRow).size();
-					console.log(count);
-					if(count > 1){
-						$(this._gridID + " table " + this._chkClass + ":checked").prop("checked", false);
-						var selected = this._grid.select();
-						$(selected).find(this._chkClass).prop("checked", true);
-						this._grid.select(selected);
-					} else {
-						$(row).removeClass("k-state-selected").removeAttr("aria-selected");
-					}
+                    $(selected).find(this._chkClass).prop("checked", true);
+                    this._grid.select(selected);
+                } else {
+                    var count = $(this._selectedRow).size();
+                    console.log(count);
+                    if (count > 1) {
+                        $(this._gridID + " table " + this._chkClass + ":checked").prop("checked", false);
+                        var selected = this._grid.select();
+                        $(selected).find(this._chkClass).prop("checked", true);
+                        this._grid.select(selected);
+                    } else {
+                        $(row).removeClass("k-state-selected").removeAttr("aria-selected");
+                    }
                 }
             } else {
                 var selected = this._grid.select();
@@ -84,62 +83,62 @@
                 } else {
                     $(this._gridID + " table " + this._chkClass + ":checked").prop("checked", false);
                 }
-                
+
                 this._grid.select(selected);
                 selected.find(this._chkClass).prop("checked", true);
             }
-			this._selectedRow = this._grid.select();
+            this._selectedRow = this._grid.select();
         },
-        _handleSelectAll: function () {
-			var checked = $(this._chkAllID).prop("checked");
-			var row = $(this._gridID + " table tbody tr");
-				if(checked){
-					this._selectRow(row);	
-				} else {
-					this._deselectRow(row);	
-				}
+        _handleSelectAll : function() {
+            var checked = $(this._chkAllID).prop("checked");
+            var row = $(this._gridID + " table tbody tr");
+            if (checked) {
+                this._selectRow(row);
+            } else {
+                this._deselectRow(row);
+            }
         },
-        _handleSelectMultiRow: function (rowSelector) {
-            if($(rowSelector).hasClass("selectedRow")) {
+        _handleSelectMultiRow : function(rowSelector) {
+            if ($(rowSelector).hasClass("selectedRow")) {
                 this._deselectRow(rowSelector);
             } else {
                 this._selectRow(rowSelector);
             }
         },
-        _selectRow: function (rowSelector) {
+        _selectRow : function(rowSelector) {
             $(rowSelector).removeClass("k-state-selected");
             $(rowSelector).addClass("selectedRow");
             $(rowSelector).attr("aria-selected", true);
-            if(this._chkClass !== "") {
+            if (this._chkClass !== "") {
                 $(rowSelector).find(this._chkClass).prop("checked", true);
             }
         },
-        _deselectRow: function (rowSelector) {
+        _deselectRow : function(rowSelector) {
             $(rowSelector).removeClass("k-state-selected");
             $(rowSelector).removeClass("selectedRow");
             $(rowSelector).attr("aria-selected", false);
-            if(this._chkClass !== "") {
+            if (this._chkClass !== "") {
                 $(rowSelector).find(this._chkClass).prop("checked", false);
             }
         },
-		_pageChange: function () {
-			var that = this;
-			$(that._chkAllID).prop("checked", false);
-			$(that._gridID + " table tr").on({click: function(e) {
+        _pageChange : function() {
+            var that = this;
+            $(that._chkAllID).prop("checked", false);
+            $(that._gridID + " table tr").on({click : function(e) {
                 that._handleSingleClick(e);
             }});
-		},
-        grid: function () {
+        },
+        grid : function() {
             return this._grid;
         },
-        options: {
-            name: "ExtGridSelectable",
-            filterHeader: "",
-            chkTemplate: "",
-            chkAllTemplate: "",
-            chkAllID: "",
-            selKendoClass: "",
-            selRowClass: ""
+        options : {
+            name : "ExtGridSelectable",
+            filterHeader : "",
+            chkTemplate : "",
+            chkAllTemplate : "",
+            chkAllID : "",
+            selKendoClass : "",
+            selRowClass : ""
         }
     });
     kendo.ui.plugin(ExtGridSelectable);
